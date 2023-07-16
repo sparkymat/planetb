fc7c602a1c180d7b183ca8d6d59ba5bccc79c380
Ajith Hussain Subayar <ajith.hussain@fastmail.com>
2023-07-17 04:18:57 +1000

    WIP

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/sparkymat/planetb
 
 go 1.20
 
-require github.com/gen2brain/raylib-go/raylib v0.0.0-20230621111137-9def9b04c9ec // indirect
+require (
+	github.com/gen2brain/raylib-go/raygui v0.0.0-20230621111137-9def9b04c9ec // indirect
+	github.com/gen2brain/raylib-go/raylib v0.0.0-20230621111137-9def9b04c9ec // indirect
+)
--- planetb.go
+++ planetb.go
@@ -1,11 +1,18 @@
 package main
 
-import rl "github.com/gen2brain/raylib-go/raylib"
+import (
+	"time"
+
+	rl "github.com/gen2brain/raylib-go/raylib"
+)
 
 func main() {
 	display := rl.GetCurrentMonitor()
+
 	width := int32(rl.GetMonitorWidth(display))
 	height := int32(rl.GetMonitorHeight(display))
+
+	rl.SetConfigFlags(rl.FlagVsyncHint)
 
 	rl.InitWindow(width, height, "raylib [core] example - basic window")
 	defer rl.CloseWindow()
@@ -13,11 +20,25 @@ func main() {
 	rl.SetTargetFPS(60)
 	rl.SetWindowSize(rl.GetMonitorWidth(display), rl.GetMonitorHeight(display))
 
-	for !rl.WindowShouldClose() {
-		rl.BeginDrawing()
+	camera := rl.NewCamera3D(
+		rl.NewVector3(0.0, -20.0, -20.0),
+		rl.NewVector3(0.0, 0.0, 0.0),
+		rl.NewVector3(0.0, 1.0, 0.0),
+		45.0,
+		rl.CameraPerspective,
+	)
 
+	for !rl.WindowShouldClose() {
+		camera.Position.X = 500.0 - float64(time.Now().UnixMilli()%1000)
+		rl.BeginDrawing()
 		rl.ClearBackground(rl.RayWhite)
-		rl.DrawText("Congrats! You created your first window!", 190, 200, 20, rl.LightGray)
+
+		rl.BeginMode3D(camera)
+
+		rl.DrawCube(rl.NewVector3(0, 0, 0), 10.0, 10.0, 10.0, rl.Green)
+
+		rl.EndMode3D()
+		rl.DrawFPS(10, 10)
 
 		rl.EndDrawing()
 	}
